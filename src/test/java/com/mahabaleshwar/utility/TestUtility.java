edfa0399b2c295f3253a6708e6159b9b8c3bd706
rohan shedge <rohan_it05@hotmail.com>
2020-01-18 15:20:35 +0530

    Added Test Utility

--- src/test/java/com/mahabaleshwar/utility/TestUtility.java
+++ src/test/java/com/mahabaleshwar/utility/TestUtility.java
@@ -1,10 +1,35 @@
 package com.mahabaleshwar.utility;
 
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import com.mahabaleshwar.FeatureSwitchActivation;
+import com.mahabaleshwar.FeatureSwitchActivationInvokedMethodListener;
+import com.mahabaleshwar.FeatureSwitch.FeatureSwitch;
+import com.mahabaleshwar.util.Utility;
+
 @Test
+@Listeners(FeatureSwitchActivationInvokedMethodListener.class)
 public class TestUtility {
 	
+	@DataProvider
+	private static Object[][] testDataConnectors() {
+		return new Object[][]
+				{
+						{"Rohan Shedge", "Rohan Shedge"},
+						{"Rohan Shedge", "Rohan Shedge"}
+				};
+	}
 	
+	@Test(dataProvider = "testDataConnectors")
+	@FeatureSwitchActivation(FeatureSwitch.DATABASECONNECTIVITY)
+	public void testOwnersName(String expected, String actuals) {
+		System.out.print("TESTING FS \n");
+		assertEquals("Rohan Shedge",Utility.getOwnerName());
+		assertEquals(expected, actuals);
+	}
 
 }
